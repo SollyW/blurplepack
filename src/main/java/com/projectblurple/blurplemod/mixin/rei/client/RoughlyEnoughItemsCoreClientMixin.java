a1a66b531e63df942bf3c071fa9ceb9c83201e0d
Solly Watkins <solly@sollyw.com>
2022-05-12 23:49:24 +1000

    Push

--- src/main/java/com/projectblurple/blurplemod/mixin/rei/client/RoughlyEnoughItemsCoreClientMixin.java
+++ src/main/java/com/projectblurple/blurplemod/mixin/rei/client/RoughlyEnoughItemsCoreClientMixin.java
@@ -1,7 +1,9 @@
 package com.projectblurple.blurplemod.mixin.rei.client;
 
+import me.shedaniel.rei.RoughlyEnoughItemsCore;
 import me.shedaniel.rei.RoughlyEnoughItemsCoreClient;
 import me.shedaniel.rei.api.client.config.ConfigObject;
+import me.shedaniel.rei.api.common.registry.ReloadStage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -14,5 +16,17 @@ public abstract class RoughlyEnoughItemsCoreClientMixin {
             remap = false)
     private static boolean doesRegisterRecipesInAnotherThread(ConfigObject instance) {
         return false;
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    @Redirect(method = "reloadPlugins",
+            at = @At(value = "INVOKE",
+                    target = "Lme/shedaniel/rei/RoughlyEnoughItemsCore;_reloadPlugins(Lme/shedaniel/rei/api/common/registry/ReloadStage;)V"),
+            remap = false)
+    private static void _reloadPlugins(ReloadStage reloadStage) {
+        try {
+            RoughlyEnoughItemsCore._reloadPlugins(reloadStage);
+        } catch (Exception ignored) {
+        }
     }
 }
